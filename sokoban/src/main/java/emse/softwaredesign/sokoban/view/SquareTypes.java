5616b446358338e58c811be6fff619a54fabb66a
Alberth Montero <alberthm@gmail.com>
2014-05-05 17:25:24 +0200

    Change inCode

--- sokoban/src/main/java/emse/softwaredesign/sokoban/view/SquareTypes.java
+++ sokoban/src/main/java/emse/softwaredesign/sokoban/view/SquareTypes.java
@@ -6,8 +6,66 @@ package emse.softwaredesign.sokoban.view;
  * This is called <i>Fake Model</i>
  *
  * @author Marek Lewandowski <marek.lewandowski@icompass.pl>
+ * @author Alberth Montero <alberthm@gmail.com>
+ * 
  * @since 01/05/14
  */
 public enum SquareTypes {
-    PLAYER, BOX, BOX_ON_LOCATION, PLAYER_ON_LOCATION, FLOOR, WALL, OUTSIDE_WALL
+    
+    /**
+     * Box On Slot
+     */
+	PLAYER_ON_LOCATION{
+		public String toString(){
+			return "B";
+		}// end toString() override
+	},
+    /**
+     * Player color piece
+     */
+    PLAYER{
+    	public String toString(){
+    		return "p";
+    	}// end toString() override
+    }, 
+    /**
+     * Box color piece
+     */
+    BOX{
+    	public String toString(){
+    		return "b";
+    	}// end of toString() override
+    },
+    /**
+     * BoxSlot color piece
+     */
+    BOX_ON_LOCATION{
+    	public String toString(){
+    		return "l";
+    	}// end of toString() override
+    }, 
+    /**
+     * Floor color piece
+     */
+    FLOOR{
+    	public String toString(){
+    		return "f";
+    	}// end of toString() override
+    }, 
+    /**
+     * Wall color piece
+     */
+    WALL{
+    	public String toString(){
+    		return "W";
+    	}// end of toString() override
+    }, 
+    /**
+     * Empty
+     */
+    OUTSIDE_WALL {
+    	public String toString(){
+    		return "-";
+    	}// end of toString() override
+    };
 }
